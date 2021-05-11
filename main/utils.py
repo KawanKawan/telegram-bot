a17e15dfe88bbecd0c4f43322e357117de4c84a9
Zheng Zikang <zhengzikangzzz@gmail.com>
2021-05-11 14:55:58 +0800

    created utils file

---
+++ main/utils.py
@@ -0,0 +1,7 @@
+def facts_to_str(user_data: Dict[str, str]) -> str:
+    facts = list()
+
+    for key, value in user_data.items():
+        facts.append(f'{key}: {value}')
+
+    return "\n".join(facts).join(['\n', '\n'])
