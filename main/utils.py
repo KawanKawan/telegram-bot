f9550a3587ec70b10250f85d8a11de253eb85449
Zheng Zikang <zhengzikangzzz@gmail.com>
2021-05-13 23:48:20 +0800

    moved dbml file

--- main/utils.py
+++ main/utils.py
@@ -4,6 +4,6 @@ def facts_to_str(user_data: Dict[str, str]) -> str:
     facts = list()
 
     for key, value in user_data.items():
-        facts.append(f'{key}: {value}')
+        facts.append(f'*{key}*: {value}')
 
     return "\n".join(facts).join(['\n', '\n'])
